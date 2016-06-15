51a3016a22016f7fc0d65449db667a7e9688cda9
David Anthony <djanthony@gmail.com>
2016-06-14 19:55:02 -0500

    Return both table and struct of data

--- bagReader.m
+++ bagReader.m
@@ -1,9 +1,14 @@
-function [bag_data] = bagReader(bag_file, topic_name)
+function [bag_data_table, bag_data_struct] = bagReader(bag_file, topic_name)
 % bagReader Reads messages from a ROS bag file
-%	Usage:	genericExtractor(bag_file, topic_name) returns a table of all
-%   the data published on the topic 'topic_name' in the bag 'bag_file'. 
+%	Usage:	bagReader(bag_file, topic_name) returns a table and struct
+%   of all the data published on the topic 'topic_name' in the bag 
+%   'bag_file'.
+%
 %   bag_file is the path to the bag file to analyze
 %   topic_name is a string containing the topic name to read
+%
+% Returns [bag_data_table, bag_data_struct]. These are representations of
+%   the data as a table and struct array, respectively.
 
 %   Copyright (c) 2016 David Anthony
 %
@@ -29,9 +34,9 @@ function [bag_data] = bagReader(bag_file, topic_name)
   end
   
   % Read the data in the bag file
-  bag_data = py.matlab_bag_helper.read_bag(bag_file, topic_name);
+  bag_data_struct = py.matlab_bag_helper.read_bag(bag_file, topic_name);
   % Convert the Python data to an array of structurs
-  bag_data = py2Matlab(bag_data);
+  bag_data_struct = py2Matlab(bag_data_struct);
   % Now make the Matlab structures a table
-  bag_data = struct2table(bag_data);
+  bag_data_table = struct2table(bag_data_struct);
 end
